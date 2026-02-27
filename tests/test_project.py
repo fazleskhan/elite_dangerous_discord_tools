8b465b5f33a734454899cbea62f212d5854297d3
Fazle Khan <fazleskhan@gmail.com>
2026-02-27 22:21:41 +0000

    renamed project.py to main.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,16 +1,16 @@
-import project
+import main
 import test_data
 
 
-def main(): ...
+def main_func(): ...
 
 
 def test_initialize_db():
-    assert project.get_all_system_names() != None
+    assert main.get_all_system_names() != None
 
 
 def test_calc_route():
-    assert project.calc_route("Sol", "Ross 248", 100) == [
+    assert main.calc_route("Sol", "Ross 248", 100) == [
         "Sol",
         "Barnard's Star",
         "61 Cygni",
@@ -20,7 +20,7 @@ def test_calc_route():
 
 def test_get_system_info():
     system_names = ["Sol", "Barnard's Star", "61 Cygni", "Ross 248"]
-    system_infos = project.get_system_info(system_names)
+    system_infos = main.get_system_info(system_names)
     # TODO looks like the order of the returned no gaurantee, so this test is not stable. Need to find a way to make it stable.
     # assert system_infos == [
     #    test_data.sol_complete_info,
